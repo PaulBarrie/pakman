985012a645022f48d281a68affa8e74873282853
Adem Mrizak <adem.mrizak@gmail.com>
2022-10-10 19:48:05 +0200

    Saving qtable

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import os
 from agents.qtable_pakman import QtablePakman
 from environment.state import State
 from pakman_window import PakmanWindow
 from environment.environment import Environment
 import maps
 import arcade
+
+SAVE_FILE = 'qtable.dat'
 
 if __name__ == '__main__':
     environment = Environment.from_str_map(maps.GAME2)
@@ -14,6 +17,10 @@ if __name__ == '__main__':
         environment.walls
     )
     agent = QtablePakman(environment.initial_pakman_position, initial_state, environment)
+    if os.path.exists(SAVE_FILE):
+        agent.load(SAVE_FILE)
     pakman = PakmanWindow(environment, agent)
     pakman.setup()
     arcade.run()
+
+    agent.save(SAVE_FILE)
